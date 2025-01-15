c05388f684f2280ae65a0573a30b9cc0195043f2
Coderyounes <younesanas2021@gmail.com>
2025-01-15 13:07:55 +0100

    feat: #18 Creation d'une class Tag entity

---
+++ Entity/Tag.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Younes\Youdemy\Entity;
+
+use Younes\Youdemy\Helpers\Validator;
+
+use Exception;
+
+class Tag
+{
+    private $tag_id;
+    private $tag_nom;
+
+    public function __construct($tag_id, $tag_nom) {
+        try {
+            $this->tag_id = $tag_id ? Validator::ValidateData($tag_id) : null;
+            $this->tag_nom = Validator::ValidateData($tag_nom);
+        } catch (Exception $e) {
+            echo "Error Tag : " . $e->getMessage();
+        }
+    }
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+
+    public  function __set($property, $value) {
+        $this->$property = $value;
+    }
+
+}
