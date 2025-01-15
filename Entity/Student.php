2e364460f42c6a4e5fde6ea7a60f8d0d4ec3ce6f
Coderyounes <younesanas2021@gmail.com>
2025-01-15 12:50:36 +0100

    feat: #12 Creation d'une class Student extended depuis person class

---
+++ Entity/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Younes\Youdemy\Entity;
+
+class Student extends Person
+{
+
+    public function __construct($user_id, $nom, $prenom, $email, $password, $fk_role_id)
+    {
+        parent::__construct($user_id, $nom, $prenom, $email, $password, $fk_role_id);
+    }
+}
