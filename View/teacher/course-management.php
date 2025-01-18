3ac7b7791430715c71def253ce599db0ed4f9198
Coderyounes <younesanas2021@gmail.com>
2025-01-19 00:47:38 +0100

    feat: impelemte a feature where teacher can delete his own course

--- View/teacher/course-management.php
+++ View/teacher/course-management.php
@@ -1,85 +1,60 @@
-<?php include_once __DIR__ . '/common/header.php'?>
-  
-    <div class="container mx-auto p-6 mt-10">
-     
-            <h1  class="text-3xl text-center  text-gray-800   font-bold mb-8">Manage Courses</h1>
+<?php include_once __DIR__ . '/common/header.php' ?>
+<div class="container mx-auto p-6 mt-10">
 
-        <div class="bg-white shadow  rounded py-4  mt-3   overflow-x-auto ">
-        
-           
-            <table class="min-w-full leading-normal  " >
-             <thead>
-                    <tr >
-                       <th class="px-5 py-3 border-b-2 text-left text-sm   font-medium   text-gray-600 "> Title  </th>
-                        <th class="px-5 py-3 border-b-2  text-left text-sm  font-medium text-gray-600 ">Category</th>
-                          <th class="px-5 py-3  border-b-2  text-left text-sm   font-medium text-gray-600"> Students Enrolled </th>
-                      <th class="px-5 py-3  border-b-2 text-left text-sm   font-medium text-gray-600  ">Created at</th>
-                      
-                    <th  class="px-5 py-3 border-b-2 text-left text-sm  font-medium  text-gray-600">Action</th>
+  <h1 class="text-3xl text-center  text-gray-800   font-bold mb-8">Manage Courses</h1>
 
-             </tr>
-                </thead>
-
-           <tbody >
-
-                  <tr >
-
-                <td  class="px-5 py-5 border-b  bg-white   text-sm text-gray-700"> 
-                       Mastering Python Programing   </td>
-                         <td  class="px-5 py-5 border-b  bg-white  text-sm  text-gray-700  ">   Web Development</td>
-
-                  <td   class="px-5 py-5 border-b  bg-white  text-sm  text-gray-700"  >155 </td>
-
-                    <td class="px-5 py-5  border-b bg-white  text-sm text-gray-700 ">  24/ 12 / 2023 </td>
+  <div class="bg-white shadow  rounded py-4  mt-3   overflow-x-auto ">
 
 
-                      <td class="px-5  py-5   border-b   bg-white  text-sm    "  >  
-
-                         <div class="flex space-x-1" >
-
-                          <a href="course-enrollment.php" class="text-blue-600  font-medium    rounded p-1 transition  hover:text-blue-800  " > View Enrollment    </a>
-                         
-                          <button class="text-yellow-500  font-medium   rounded  p-1    hover:text-yellow-600 transition  "> <i class="fas fa-edit">  </i></button>
-                           <button class="text-red-500  font-medium   rounded   p-1    hover:text-red-600   transition">   <i  class="fas fa-trash-alt">  </i>   </button>
-                        
-                    
-                      </div>
-
-                    </td>
+    <table class="min-w-full leading-normal  ">
+      <thead>
+        <tr>
+          <th class="px-5 py-3 border-b-2 text-left text-sm   font-medium   text-gray-600 "> Title </th>
+          <th class="px-5 py-3 border-b-2  text-left text-sm  font-medium text-gray-600 ">Category</th>
+          <th class="px-5 py-3  border-b-2  text-left text-sm   font-medium text-gray-600"> Students Enrolled </th>
+          <th class="px-5 py-3 border-b-2 text-left text-sm  font-medium  text-gray-600">Action</th>
 
         </tr>
+      </thead>
 
-        <tr  >
+      <tbody>
+<?php foreach ($courses as $course) : ?>
+        <tr>
+          <td class="px-5 py-5 border-b  bg-white   text-sm text-gray-700"> <?= $course->course_nom ?></td>
+          <td class="px-5 py-5 border-b  bg-white  text-sm  text-gray-700  "> <?= $course->categorie_nom ?></td>
 
-          <td   class="px-5  py-5 border-b bg-white  text-sm  text-gray-700 "> React Native Crash Course</td>
-              <td  class="px-5 py-5  border-b   bg-white text-sm  text-gray-700">  Mobile Development </td>
-                  <td   class="px-5 py-5 border-b  bg-white   text-sm  text-gray-700">   80</td>
-             <td  class="px-5  py-5  border-b  bg-white text-sm text-gray-700">  02 / 02 / 2024  </td>
-    
+          <td class="px-5 py-5 border-b  bg-white  text-sm  text-gray-700"><?= $course->Students_Enrolled ?> </td>
 
-            <td class="px-5 py-5 border-b bg-white text-sm   " >
-          
-           
-             <div class="flex  space-x-1">
-         
-                  <a href="course-enrollment.php" class="text-blue-600  font-medium rounded  p-1 transition  hover:text-blue-800   " > View Enrollment    </a>
-             
-                      <button  class="text-yellow-500  font-medium   rounded   p-1    hover:text-yellow-600   transition">    <i  class="fas fa-edit">  </i>   </button>
+          <td class="px-5  py-5   border-b   bg-white  text-sm    ">
 
-                        <button   class="text-red-500 font-medium    rounded  p-1   hover:text-red-600  transition ">  <i   class="fas fa-trash-alt"   >   </i> </button>
-                     
-                </div>
+            <div class="flex space-x-1">
 
-             </td>
-  
-           </tr>
+              <a href="/teacher/courses/enrollments/<?= $course->course_id ?>" class="text-blue-600  font-medium    rounded p-1 transition  hover:text-blue-800  "> View Enrollment </a>
 
-       </tbody>
-
-      </table>
-
- </div>
+              <button class="text-yellow-500  font-medium   rounded  p-1    hover:text-yellow-600 transition  "> <i class="fas fa-edit"> </i></button>
+                <form action="/teacher/course/delete" method="POST">
+                    <input id="delete" type="hidden" name="course_id" class="   text-red-600   hover:text-red-700  transition    rounded   p-1" value="<?= $course->course_id ?>" >
+                    <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
+                    <button type="submit" class="text-red-600 hover:text-red-700 transition rounded p-1">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </form>
 
 
-    </div>
+            </div>
+
+          </td>
+
+        </tr>
+<?php endforeach; ?>
+
+
+      </tbody>
+
+    </table>
+
+  </div>
+
+
+</div>
 <?php include_once  __DIR__ . '/common/footer.php' ?>
