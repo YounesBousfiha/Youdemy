e8e7b66bb95fe7b4dce2598cbacb7d45eae1dec1
Coderyounes <younesanas2021@gmail.com>
2025-01-19 00:49:55 +0100

    feat: create a method in EnrollmentController.php Where we can display student enrolled in a coruse

--- Controllers/EnrollmentController.php
+++ Controllers/EnrollmentController.php
@@ -2,7 +2,33 @@
 
 namespace Younes\Youdemy\Controllers;
 
+use Younes\Youdemy\Config\DBConnection;
+use Younes\Youdemy\DAO\EnrollmentDAO;
+use Younes\Youdemy\Helpers\Session;
+
 class EnrollmentController
 {
+    private $session;
+    private $db;
+
+    private $enrollmentdao;
+
+
+    public function  __construct() {
+        $this->db = DBConnection::getConnection()->conn;
+        $this->session = new Session();
+        $this->enrollmentdao  = new EnrollmentDAO($this->db);
+    }
+
+    public function TeacherCourseManager($id) {
+        $course_id = htmlspecialchars($id);
+
+        $enrollments = $this->enrollmentdao->studentEnrolledByTeacher($course_id);
+
+        require_once __DIR__ . '/../View/teacher/course-enrollment.php';
+    }
+
+
+
 
 }
