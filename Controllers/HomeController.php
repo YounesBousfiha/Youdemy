341336f313909a8e6316fae858b724ec2ff0e05f
Coderyounes <younesanas2021@gmail.com>
2025-01-19 22:01:34 +0100

    chore: update an existing method in HomeController.php

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace Younes\Youdemy\Controllers;
 
+use Parsedown;
 use Younes\Youdemy\Config\DBConnection;
 use Younes\Youdemy\DAO\CourseDAO;
+use Younes\Youdemy\DAO\EnrollmentDAO;
 use Younes\Youdemy\Helpers\Session;
 use Younes\Youdemy\DAO\CategorieDAO;
 
@@ -58,10 +60,37 @@ class HomeController
 
     public function courseDetails($id) {
         try {
+            $fk_user_id = $_SESSION['user_id'];
             $course_id = Validator::ValidateData($id);
             $courseDAO = new CourseDAO($this->db);
-            $courseDATA = $courseDAO->read($course_id);
+            $enrollmentDao = new EnrollmentDAO($this->db);
+            $enrollments = $enrollmentDao->isEnroll($fk_user_id, $course_id);
+            $courseDATA = $courseDAO->CourseDetails($course_id);
             include_once __DIR__ . '/../View/student/course-details.php';
+        } catch (Exception $e) {
+            $this->session->set('Error', $e->getMessage());
+            return null;
+        }
+    }
+
+    public function courseContent($id) {
+        try {
+            $parsedown = new Parsedown();
+            $course_id = Validator::ValidateData($id);
+            $courseDAO = new CourseDAO($this->db);
+            $courseDATA = $courseDAO->CourseDetails($course_id);
+
+            $data = substr($courseDATA[0]->course_content, 1, -1);
+            $data2 = substr($data, 0, -1);
+
+            $html = $parsedown->text($data2);
+
+            $html = str_replace("<h1>", '<h1 class="text-6xl font-bold my-4">', $html);
+            $html = str_replace("<h2>", '<h2 class="text-3xl font-semibold my-3">', $html);
+            $html = str_replace("<h3>", '<h2 class="text-2xl font-semibold my-3">', $html);
+            $html = str_replace("<code>", '<code class="bg-gray-200 p-1 rounded">', $html);
+
+            include_once __DIR__ . '/../View/student/course-content.php';
         } catch (Exception $e) {
             $this->session->set('Error', $e->getMessage());
             return null;
