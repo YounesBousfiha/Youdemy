5ed759167a7fd6d8c98b334c4b14de5f22500ec5
Coderyounes <younesanas2021@gmail.com>
2025-01-19 03:12:33 +0100

    feat: update the course entity so it can handle the update

--- Entity/Course.php
+++ Entity/Course.php
@@ -21,15 +21,15 @@ class Course
     public function __construct($course_id, $course_nom,$course_desc , $course_miniature, $course_visibility, $course_status, $course_type, $course_content, $fk_user_id, $fk_categorie_id) {
         try {
             $this->course_id = $course_id ? Validator::ValidateData($course_id) : null;
-            $this->course_nom = Validator::ValidateData($course_nom);
-            $this->course_desc = Validator::ValidateData($course_desc);
+            $this->course_nom = $course_nom ? Validator::ValidateData($course_nom) : null;
+            $this->course_desc = $course_desc ? Validator::ValidateData($course_desc) : null;
             $this->course_miniature = $course_miniature ? Validator::ValidateImage($course_miniature) : null;
-            $this->course_visibility = Validator::ValidateData($course_visibility);
-            $this->course_status = Validator::ValidateData($course_status);
-            $this->course_type = Validator::ValidateData($course_type);
-            $this->course_content = Validator::ValidateData($course_content);
-            $this->fk_user_id = Validator::ValidateData($fk_user_id);
-            $this->fk_categorie_id = Validator::ValidateData($fk_categorie_id);
+            $this->course_visibility = $course_visibility ? Validator::ValidateData($course_visibility) : null;
+            $this->course_status = $course_status ? Validator::ValidateData($course_status) : null;
+            $this->course_type = $course_type ? Validator::ValidateData($course_type) : null;
+            $this->course_content = $course_content ? Validator::ValidateData($course_content) : null;
+            $this->fk_user_id = $fk_user_id ? Validator::ValidateData($fk_user_id) : null;
+            $this->fk_categorie_id = $fk_categorie_id ? Validator::ValidateData($fk_categorie_id) : null;
         } catch(\Exception $e) {
             echo 'Course Error: '. $e->getMessage();
         }
