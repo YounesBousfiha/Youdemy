8e0569c65ebefd87ca7376830a708ba3c2f4cf1f
Coderyounes <younesanas2021@gmail.com>
2025-01-19 22:44:54 +0100

    feat: impove the course-content page to handle the coruse content if video or text

--- View/student/course-content.php
+++ View/student/course-content.php
@@ -1,4 +1,5 @@
 <?php include_once  __DIR__ . '/common/header.php'; ?>
+<?php var_dump($courseDATA) ?>
 <div class="container mx-auto p-4">
     <div class="bg-white shadow rounded-lg overflow-hidden">
         <div class="px-6 py-4 flex flex-col items-center">
@@ -8,13 +9,22 @@
             </p>
         </div>
 
+
         <!-- Condition if the Course type Text Or Video  -->
+        <?php if($courseDATA[0]->course_type === 'text') : ?>
         <div class="px-6 py-4 flex flex-col items-center">
             <div class="font-bold text-xl mb-2">Course Content</div>
             <div class="prose max-w-none">
                 <?= $html ?>
             </div>
         </div>
+        <?php else : ?>
+        <div class="flex justify-center">
+            <iframe width="853" height="480" src="<?= $courseDATA[0]->course_content ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        <?php endif; ?>
+
+
 
 
         <div class="px-6 py-4 bg-gray-50">
@@ -56,10 +66,11 @@
             <div class="mt-4">
                 <textarea id="comment-text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Add your comment..."></textarea>
             </div>
-            <div class="mt-2">
+            <div class="mt-2 flex justify-between">
                 <button id="submit-comment" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                     Submit Comment
                 </button>
+
             </div>
         </div>
     </div>
